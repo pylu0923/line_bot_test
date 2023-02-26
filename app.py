9e95749f0231c4d71d116dc4795d6b9375c6db6e
pylu0923 <47583952+pylu0923@users.noreply.github.com>
2023-02-26 23:32:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, QuickReply, QuickReplyButton,URIAction, LocationAction
 )
 
 app = Flask(__name__)
